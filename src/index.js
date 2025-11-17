5ecb73426d941cde66571a7862d29cc31174dfa1
chevalasandeep8374 <167118416+chevalasandeep8374@users.noreply.github.com>
2025-11-17 21:59:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Counter from './component/counter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    
   </React.StrictMode>
 );
 
