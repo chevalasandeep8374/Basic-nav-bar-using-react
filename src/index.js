8e0151c4bac788e0ec259647c3b2483734317f92
chevalasandeep8374 <167118416+chevalasandeep8374@users.noreply.github.com>
2025-11-18 10:22:15 +0530

    Correct root element ID for ReactDOM
    Fix root element ID in ReactDOM.createRoot call.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Counter from './component/counter';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById(''));
 root.render(
   <React.StrictMode>
     <App/>
